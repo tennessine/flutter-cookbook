c656a2690c4b92dd73b185aeebc5e201210156a3
gekongfei <389443626@qq.com>
2019-01-24 01:35:45 +0800

    update

---
+++ lib/src/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'screens/home.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'demo',
+      home: Home(
+        title: 'demo',
+      ),
+    );
+  }
+}
---
+++ lib/src/screens/home.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  final String title;
+
+  Home({Key key, this.title}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool _visible = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _visible ? 1.0 : 0.0,
+          duration: Duration(milliseconds: 500),
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.green,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.flip),
+        onPressed: () {
+          setState(() {
+            _visible = !_visible;
+          });
+        },
+      ),
+    );
+  }
+}
