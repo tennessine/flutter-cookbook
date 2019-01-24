41b12e41f2cc86ee81ba234036fcb489ac48cd70
gekongfei <389443626@qq.com>
2019-01-24 21:52:24 +0800

    update

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -7,8 +7,17 @@ class App extends StatelessWidget {
       title: 'demo',
       home: Scaffold(
         appBar: AppBar(),
-        body: Center(
-          child: Text('demo'),
+        body: Container(
+          margin: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.blue),
+          ),
+          child: Image.network(
+            'http://localhost/gif.gif',
+            width: 100,
+            height: 100,
+            fit: BoxFit.scaleDown,
+          ),
         ),
       ),
     );
