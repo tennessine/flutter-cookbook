0efb8ca021ac37fe0e0f811174b686f7d47d1c40
gekongfei <389443626@qq.com>
2019-01-24 22:34:02 +0800

    update

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
@@ -8,7 +9,10 @@ class App extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(),
         body: Center(
-          child: Text('demo'),
+          child: CachedNetworkImage(
+              placeholder: CircularProgressIndicator(),
+              imageUrl:
+                  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548350092752&di=318276df1192020f031cf8fb57761d7b&imgtype=0&src=http%3A%2F%2Fimg.ph.126.net%2FRCC5BQWsRB4nQsvtZA5rDg%3D%3D%2F649081296311594384.jpg'),
         ),
       ),
     );
