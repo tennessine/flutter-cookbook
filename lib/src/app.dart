c897b9863c13761542fd0b8d1ec07ea2d6a0e3a3
gekongfei <389443626@qq.com>
2019-01-24 23:02:46 +0800

    update

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -7,8 +7,27 @@ class App extends StatelessWidget {
       title: 'demo',
       home: Scaffold(
         appBar: AppBar(),
-        body: Center(
-          child: Text('demo'),
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text('Map'),
+              subtitle: Text('This is a map'),
+              trailing: Icon(Icons.chevron_right),
+            ),
+            ListTile(
+              leading: Icon(Icons.photo_album),
+              title: Text('Album'),
+              subtitle: Text('This is a album'),
+              trailing: Icon(Icons.chevron_right),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Phone'),
+              subtitle: Text('This is a phone'),
+              trailing: Icon(Icons.chevron_right),
+            )
+          ],
         ),
       ),
     );
