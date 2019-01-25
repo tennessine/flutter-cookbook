150d6425c4e3d8e992508b74b29bbcf305fb00bd
gekongfei <389443626@qq.com>
2019-01-25 20:01:43 +0800

    update

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -8,8 +8,29 @@ class App extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(),
         body: Center(
-          child: Text('demo'),
+          child: Button(),
         ),
+      ),
+    );
+  }
+}
+
+class Button extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        final snackBar = SnackBar(content: Text("Tap"));
+
+        Scaffold.of(context).showSnackBar(snackBar);
+      },
+      child: Container(
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: Theme.of(context).buttonColor,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Text('My Button'),
       ),
     );
   }
